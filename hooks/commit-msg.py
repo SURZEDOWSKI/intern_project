c02892316ce34fc50934e07da5ccff0d824b14fc
surzedowski <szymon.urzedowski@capgemini.com>
2022-08-18 15:44:41 +0200

    test(all-hooks): this commit follows the rules!

--- hooks/commit-msg.py
+++ hooks/commit-msg.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
+
 import sys
 import re
 
 ptrn = re.compile("^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test){1}(\(([\w\-.]+)\))?(!)?: ([\w ])+([\s\S]*)")
 
-with open(r"COMMIT_EDITMSG", 'r+') as f:
+with open(r".\\hooks\\COMMIT_EDITMSG", 'r+') as f:
         msg = f.readline()
         print(msg)
 if not ptrn.match(msg):

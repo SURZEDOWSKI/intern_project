f3ed3c1c8ecf9f7855e3b2a394b0a87232ee18f2
surzedowski <szymon.urzedowski@capgemini.com>
2022-09-14 15:15:22 +0200

    feat(database): db container and app connection

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from typing import List
 from fastapi import FastAPI, status, HTTPException, Query
 from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:pass@localhost:5432/postgres"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 class User(BaseModel):
     id: int
